2e5d29d8f631aab95895ac668383340c779b9d3d
Hyun <hwn123h@naver.com>
2024-03-05 23:20:59 +0900

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
-import { legacy_createStore as createStore } from "redux";
-import rootReducer from './reducer';
+import { createStore } from 'redux';
+import rootReducer from './reducer.js';
 
 const store = createStore(rootReducer);
-
-export default store
+export default store;
